cb375cf495dc0a231196199664a9869f0cd120da
ALLANMACHOGU <114505966+ALLANMACHOGU@users.noreply.github.com>
2024-10-16 16:19:45 +0000

    website/cc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,9 @@ import "../styles/styles.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
+
+
+
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
@@ -14,4 +17,7 @@ if (typeof window !== "undefined") {
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
+
+
+
 
