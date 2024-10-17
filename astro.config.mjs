aa566b1eadec536a0cb843d6b63e1dc1bad8b284
ALLANMACHOGU <114505966+ALLANMACHOGU@users.noreply.github.com>
2024-10-17 12:34:04 +0000

    Changed public url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ export default defineConfig({
   vite: {
     build: {
       minify: false,
+      /* Change this to match YOUR portfolio */
+      base: "https://thinkle-iacs.github.io/apcsp-portfolio-demo/",
     },
   },
 });
